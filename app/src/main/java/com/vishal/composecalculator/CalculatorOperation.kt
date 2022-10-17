a1cb9fd9a014116fb8c707fe7830a6b5c7713a08
vishal2376 <vishalsingh2376@gmail.com>
2022-10-17 15:20:32 +0530

    sealed and data class added for view model

---
+++ app/src/main/java/com/vishal/composecalculator/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package com.vishal.composecalculator
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add : CalculatorOperation("+")
+    object Subtract : CalculatorOperation("-")
+    object Multiply : CalculatorOperation("*")
+    object Divide : CalculatorOperation("/")
+}
