278e0ac8d858e8814b326139ae28c033ad3252ab
vishal2376 <vishalsingh2376@gmail.com>
2022-10-17 16:25:12 +0530

    button onclick action implemented

--- app/src/main/java/com/vishal/composecalculator/CalculatorViewModel.kt
+++ app/src/main/java/com/vishal/composecalculator/CalculatorViewModel.kt
@@ -1,11 +1,45 @@
 package com.vishal.composecalculator
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import java.nio.file.Files.delete
 
 class CalculatorViewModel : ViewModel() {
     var state by mutableStateOf(CalculatorState())
         private set //it means we can't change state outside the class but can read state var
+
+    fun onAction(actions: CalculatorActions) {
+        when (actions) {
+            is CalculatorActions.Number -> enterNumber(actions.number)
+            is CalculatorActions.Decimal -> enterDecimal()
+            is CalculatorActions.Clear -> state = CalculatorState()
+            is CalculatorActions.Delete -> performDeletion()
+            is CalculatorActions.Operation -> enterOperation(actions.operation)
+            is CalculatorActions.Calculate -> preformCalculation()
+        }
+    }
+
+    private fun enterOperation(operation: CalculatorOperation) {
+
+    }
+
+    private fun preformCalculation() {
+
+    }
+
+    private fun performDeletion() {
+
+    }
+
+    private fun enterDecimal() {
+
+    }
+
+    private fun enterNumber(number: Int) {
+        Log.e("@@@",number.toString())
+    }
+
 }
--- app/src/main/java/com/vishal/composecalculator/MainActivity.kt
+++ app/src/main/java/com/vishal/composecalculator/MainActivity.kt
@@ -3,16 +3,14 @@ package com.vishal.composecalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vishal.composecalculator.ui.theme.ComposeCalculatorTheme
+import com.vishal.composecalculator.ui.theme.MediumGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +23,16 @@ class MainActivity : ComponentActivity() {
 
                 //space between all buttons
                 val buttonSpacing = 8.dp
+
+                //Calculator UI
+                Calculator(
+                    state = state,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp),
+                    onActions = viewModel::onAction
+                )
             }
         }
     }
